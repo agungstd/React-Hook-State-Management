01d8910eee7ab531b44e1cb1b0519ac0cf7f9007
agungstd <apakatadunia11@gmail.com>
2025-03-04 10:24:21 +0700

    Add comments to Parent1 and combineReducers code

--- 01-react-props/src/component/Parent1.jsx
+++ 01-react-props/src/component/Parent1.jsx
@@ -1,8 +1,14 @@
+// Importing the Parent2 component from the Parent2.jsx file
 import Parent2 from "./Parent2.jsx";
+// Importing the PropTypes library to define prop types for the component
 import PropTypes from "prop-types";
 
+// Defining the Parent1 functional component, which takes 'nama' as a prop
 function Parent1({ nama }) {
   return (
+    // Returning a JSX element containing an <h1> tag
+    // Inside the <h1> tag, there's a <div> element with text "Parent1"
+    // and the Parent2 component, passing the 'nama' prop to it
     <h1>
       <div>Parent1</div>
       <Parent2 nama={nama} />
@@ -10,8 +16,11 @@ function Parent1({ nama }) {
   );
 }
 
+// Defining the prop types for the Parent1 component
 Parent1.propTypes = {
+  // The 'nama' prop is expected to be a string
   nama: PropTypes.string,
 };
 
+// Exporting the Parent1 component as the default export of this module
 export default Parent1;
--- 02-react-hook/src/reducers/index.jsx
+++ 02-react-hook/src/reducers/index.jsx
@@ -1,23 +1,31 @@
+// Importing the kontakReducers from the kontak.js file
 import kontakReducers from "./kontak";
+
+// Defining the initial state of the application
 const initialState = {
-  getKontakResult: false,
-  getKontakLoading: false,
-  getKontakError: false,
+  getKontakResult: false, // Holds the result of getting contacts (initially false)
+  getKontakLoading: false, // Holds the loading state of getting contacts (initially false)
+  getKontakError: false,   // Holds the error state of getting contacts (initially false)
 };
 
+// Defining the combineReducers function, which takes a dictionary of reducers as input
 const combineReducers = (reducers) => {
   return (state, action) => {
+    // Using reduce to iterate over each property in the reducers object
     return Object.keys(reducers).reduce((acc, prop) => {
+      // Combining each reducer's result into the accumulated state
       return {
         ...acc,
         ...reducers[prop]({ [prop]: state[prop] }, action),
       };
-    }, state);
+    }, state); // The initial value of the accumulator is the current state
   };
 };
 
+// Combining reducers into a single appReducers function
 const appReducers = combineReducers({
   kontakReducers,
 });
 
+// Exporting the initial state, appReducers, and combineReducers for use in other parts of the application
 export { initialState, appReducers, combineReducers };
